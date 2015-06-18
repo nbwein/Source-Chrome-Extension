4608d10975cd11fe0d48f356d14b7a746ea04ad6
Maddie Cripps <madeline.cripps@stellaservice.com>
2015-06-18 10:22:08 -0400

    initial commit

---
+++ scripts/greeting.js
@@ -0,0 +1,55 @@
+function xhrWithAuth(method, url, interactive, callback) {
+    var access_token;
+
+    var retry = true;
+
+    getToken();
+
+    function getToken() {
+      chrome.identity.getAuthToken({ interactive: interactive }, function(token) {
+        if (chrome.runtime.lastError) {
+          callback(chrome.runtime.lastError);
+          return;
+        }
+
+        access_token = token;
+        requestStart();
+      });
+    }
+
+    function requestStart() {
+      var xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+      xhr.onload = requestComplete;
+      xhr.send();
+    }
+
+    function requestComplete() {
+      if (this.status == 401 && retry) {
+        retry = false;
+        chrome.identity.removeCachedAuthToken({ token: access_token },
+                                              getToken);
+      } else {
+        callback(null, this.status, this.response);
+      }
+    }
+  }
+
+  function getUserInfo(interactive) {
+    xhrWithAuth('GET',
+                'https://www.googleapis.com/plus/v1/people/me',
+                interactive,
+                onUserInfoFetched);
+  }
+
+chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+  if (chrome.runtime.lastError) {
+    sampleSupport.log(chrome.runtime.lastError);
+    changeState(STATE_START);
+  } else {
+    sampleSupport.log('Token acquired:'+token+
+      '. See chrome://identity-internals for details.');
+    changeState(STATE_AUTHTOKEN_ACQUIRED);
+  }
+});
