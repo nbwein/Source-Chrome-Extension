9b78d6ddb9bc6845d3a29eccff59053e2da4ad20
Nick Wein <nick.wein@stellaservice.com>
2015-06-23 10:33:17 -0400

    added weather

---
+++ scripts/weather.js
@@ -0,0 +1,35 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getWeather);
+    } else { 
+        document.getElementById("weather").innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function getWeather(position) {
+  var lat = position.coords.latitude;
+  var lon = position.coords.longitude;
+  var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon;
+  console.log(url);
+  var jsonObj;
+  var icon;
+  var http_request = new XMLHttpRequest();
+  http_request.onreadystatechange = function(){
+
+      if (http_request.readyState == 4)
+      {
+        jsonObj = JSON.parse(http_request.responseText);
+
+        var img = document.createElement("img");
+        img.src = "http://openweathermap.org/img/w/" + jsonObj.weather[0].icon + ".png";
+        document.getElementById("main").appendChild(img);
+
+        var fahrenheit = Math.round((parseInt(jsonObj.main.temp) * 1.8) - 459.67);
+        document.getElementById("weather").innerHTML = "Current temperature: " + fahrenheit + "&deg, " + jsonObj.weather[0].main;
+      }
+   }
+   http_request.open("GET", url, true);
+   http_request.send();
+}
+
+getLocation();
