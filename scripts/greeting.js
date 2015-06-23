66f862ab3b1b27b3c4723be7104e13484f042bca
Maddie Cripps <madeline.cripps@stellaservice.com>
2015-06-23 16:33:44 -0400

    fixed manifest

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -221,6 +221,25 @@ function appendPre(message) {
 
 
 function tryPrivate(){
+
+chrome.webRequest.onHeadersReceived.addListener(
+    function(info) {
+        var headers = info.responseHeaders;
+        for (var i=headers.length-1; i>=0; --i) {
+            var header = headers[i].name.toLowerCase();
+            if (header == 'x-frame-options' || header == 'frame-options') {
+                headers.splice(i, 1); // Remove header
+            }
+        }
+        return {responseHeaders: headers};
+    },
+    {
+        urls: [ '*://*/*' ], // Pattern to match all http(s) pages
+        types: [ 'sub_frame' ]
+    },
+    ['blocking', 'responseHeaders']
+);
+
 document.getElementById("forum_embed").src =
   "https://groups.google.com/forum/embed/?place=forum/test-feed-private" +
   "&showsearch=true&showpopout=true&parenturl=" +
