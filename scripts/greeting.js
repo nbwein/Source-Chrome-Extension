8b8cc580f8d51c12be30fd8c6e0206370ee5519c
Nick Wein <nick.wein@stellaservice.com>
2015-06-24 17:51:44 -0400

    added shoutouts

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -17,7 +17,6 @@
   function enableButton(button) {
     button.removeAttribute('disabled');
   }
-
   function changeState(newState) {
     state = newState;
     switch (state) {
@@ -40,46 +39,49 @@
     }
   }*/
 
-var access_global;
+  var access_global;
 
-var googlePlusUserLoader  = (function() {
-function xhrWithAuth(method, url, interactive, callback, params) {
-    var access_token;
+  var googlePlusUserLoader  = (function() {
+    function xhrWithAuth(method, url, interactive, callback, params) {
+      
+      var access_token;
 
-    var retry = true;
+      var retry = true;
+      getToken();
 
-    getToken();
+      function getToken() {
 
-    function getToken() {
-      chrome.identity.getAuthToken({ interactive: true}, function(token) {
-       if (chrome.runtime.lastError) {
+        chrome.identity.getAuthToken({ interactive: true}, function(token) {
+         if (chrome.runtime.lastError) {
           callback(chrome.runtime.lastError);
           console.log(chrome.runtime.lastError);
           return;
         }
 
         access_token = token;
-	access_global = token;
+        access_global = token;
+
         requestStart();
       });
-    }
+      }
 
-    function requestStart() {
-      var xhr = new XMLHttpRequest();
-      xhr.open(method, url);
-      xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
-      xhr.onload = requestComplete;
-      if (params == ''){
-	xhr.send();
-	}
-      else {xhr.send(params);}
-    }
+      function requestStart() {
 
-    function requestComplete() {
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+        xhr.onload = requestComplete;
+        if (params == ''){
+         xhr.send();
+       }
+       else {xhr.send(params);}
+     }
+
+     function requestComplete() {
       if (this.status == 401 && retry) {
         retry = false;
         chrome.identity.removeCachedAuthToken({ token: access_token },
-                                              getToken);
+          getToken);
       } else {
         callback(null, this.status, this.response);
       }
@@ -88,18 +90,19 @@ function xhrWithAuth(method, url, interactive, callback, params) {
 
   function getUserInfo(interactive) {
     xhrWithAuth('GET',
-                'https://www.googleapis.com/plus/v1/people/me',
-                interactive,
-                onUserInfoFetched,
-    		'');
+      'https://www.googleapis.com/plus/v1/people/me',
+      interactive,
+      onUserInfoFetched,
+      '');
     /*xhrWithAuth('GET', 
 	        'https://www.googleapis.com/gmail/v1/users/me/drafts',
 		interactive,
 		onGmailInfoFetched); */
-  }
+}
 
-  function onUserInfoFetched(error, status, response) {
-    if (!error && status == 200) {
+function onUserInfoFetched(error, status, response) {
+  if (!error && status == 200) {
+      console.log("here");
       //changeState(STATE_AUTHTOKEN_ACQUIRED);
       //sampleSupport.log(response);
       var user_info = JSON.parse(response);
@@ -108,12 +111,14 @@ function xhrWithAuth(method, url, interactive, callback, params) {
       console.log(response);
       console.log("error on user info fetch");
 	//changeState(STATE_START);
-    }
-  }
+}
+}
 
-  function populateUserInfo(user_info) {
-    main_greeting.innerHTML = "Welcome, " + user_info.name.givenName;
-  }
+function populateUserInfo(user_info) {
+  main_greeting.innerHTML = "Welcome, " + user_info.name.givenName;
+  console.log(user_info.name.givenName);
+  console.log("HERE");
+}
 
  /*function onGmailInfoFetched(error, status, response) {
 	if (!error && status == 200) {
@@ -125,32 +130,51 @@ function xhrWithAuth(method, url, interactive, callback, params) {
         //changeState(STATE_START);
     }
 
-}*/
+  }*/
 
-function encodeURL(str){
+  function encodeURL(str){
     return str.replace(/\+/g, '-').replace(/\//g, '_').replace(/\=+$/, '');
-}
+  }
 
-function sendEmail(){
-        var message = $("#message-text").val();
-	var subject = Math.trunc(Math.random()*1000).toString();
-	console.log(subject);
-	var params = encodeURL(btoa("From: me\r\nTo:" + "test-feed@googlegroups.com" + "\r\nSubject:"+ subject + "\r\n\r\n" + message));
-	var numBytes = (params.length).toString();
-	$.ajax({
-		type: "POST",
-		url: "https://www.googleapis.com/gmail/v1/users/me/messages/send",
-     		contentType: "application/json",
-      		dataType: "json",
-		beforeSend: function(xhr, settings) {
-        		xhr.setRequestHeader('Authorization','Bearer ' + access_global);
-      		},
-		data: JSON.stringify({"raw": params})
-	});
-	$("#message-text").val('');
-	$("#message-board").empty();
-	loadFeed();
-	
+  function sendEmail(){
+    var message = $("#message-text").val();
+    var subject = Math.trunc(Math.random()*1000).toString();
+    console.log(subject);
+    var params = encodeURL(btoa("From: me\r\nTo:" + "test-feed@googlegroups.com" + "\r\nSubject:"+ subject + "\r\n\r\n" + message));
+    var numBytes = (params.length).toString();
+    $.ajax({
+      type: "POST",
+      url: "https://www.googleapis.com/gmail/v1/users/me/messages/send",
+      contentType: "application/json",
+      dataType: "json",
+      beforeSend: function(xhr, settings) {
+        xhr.setRequestHeader('Authorization','Bearer ' + access_global);
+      },
+      data: JSON.stringify({"raw": params})
+    });
+    $("#message-text").val('');
+    $("#message-board").empty();
+    loadFeed();
+  }
+
+  function sendShoutout(){
+    var message = $("#shoutout-text").val();
+    var subject = Math.trunc(Math.random()*1000).toString();
+    var params = encodeURL(btoa("From: me\r\nTo:" + "shoutout-feed@googlegroups.com" + "\r\nSubject:"+ subject + "\r\n\r\n" + message));
+    var numBytes = (params.length).toString();
+    $.ajax({
+      type: "POST",
+      url: "https://www.googleapis.com/gmail/v1/users/me/messages/send",
+      contentType: "application/json",
+      dataType: "json",
+      beforeSend: function(xhr, settings) {
+        xhr.setRequestHeader('Authorization','Bearer ' + access_global);
+      },
+      data: JSON.stringify({"raw": params})
+    });
+    $("#shoutout-text").val('');
+  }
+
 	/*var http = new XMLHttpRequest();
 	http.open("POST", 'https://www.googleapis.com/gmail/v1/users/me/messages/send');
 	http.setRequestHeader('Authorization', 'Bearer ' + access_global);
@@ -170,86 +194,56 @@ function sendEmail(){
         });
         request.execute(function(data){
                 console.log(data)
-        }); */
+              }); */
        /* }, 
-	encodeURIComponent("raw="+btoa("From: me\r\nTo:" + "test-feed@googlegroups.com" + "\r\nSubject:"+ "subject" + "\r\n\r\n" + "message"))); */ 
+       encodeURIComponent("raw="+btoa("From: me\r\nTo:" + "test-feed@googlegroups.com" + "\r\nSubject:"+ "subject" + "\r\n\r\n" + "message"))); */ 
 
-}
+
 
 function getCalendarSession(){
 	gapi.auth.authorize(
-          {client_id: '847225712349-afs3e8aobcglbi1ml1gjkcr764ri1jvk.apps.googleusercontent.com', scope: ['https://www.googleapis.com/auth/calendar.readonly'], immediate: true},
-          getCalendar);
-        return false;
-      }
+    {client_id: '847225712349-afs3e8aobcglbi1ml1gjkcr764ri1jvk.apps.googleusercontent.com', scope: ['https://www.googleapis.com/auth/calendar.readonly'], immediate: true},
+    getCalendar);
+  return false;
+}
 
 
 function getCalendar() {
 	var request = gapi.client.calendar.events.list({
-	  'calendarId': 'primary',
-          'timeMin': (new Date()).toISOString(),
-          'showDeleted': false,
-          'singleEvents': true,
-          'maxResults': 5,
-          'orderBy': 'startTime'
-	});
+   'calendarId': 'primary',
+   'timeMin': (new Date()).toISOString(),
+   'showDeleted': false,
+   'singleEvents': true,
+   'maxResults': 5,
+   'orderBy': 'startTime'
+ });
 	request.execute(function(resp){
 		var events = resp.items;
 		
 		appendPre('Upcoming Events: ');
 
-          if (events.length > 0) {
-            for (var i = 0; i < events.length; i++) {
-              var event = events[i];
-              var when = event.start.dateTime;
-              if (!when) {
-                when = event.start.date;
-              }
-              appendPre(event.summary + ' (' + when + ')')
-            }
-          } else {
-            appendPre('No upcoming events found.');
-          }
-	});
+    if (events.length > 0) {
+      for (var i = 0; i < events.length; i++) {
+        var event = events[i];
+        var when = event.start.dateTime;
+        if (!when) {
+          when = event.start.date;
+        }
+        appendPre(event.summary + ' (' + when + ')')
+      }
+    } else {
+      appendPre('No upcoming events found.');
+    }
+  });
 }
 
 function appendPre(message) {
-        var pre = document.getElementById('upcoming-events');
-        var textContent = document.createTextNode(message + '\n');
-        pre.appendChild(textContent);
-      }
-
-
-function tryPrivate(){
-
-chrome.webRequest.onHeadersReceived.addListener(
-    function(info) {
-        var headers = info.responseHeaders;
-        for (var i=headers.length-1; i>=0; --i) {
-            var header = headers[i].name.toLowerCase();
-            if (header == 'x-frame-options' || header == 'frame-options') {
-                headers.splice(i, 1); // Remove header
-            }
-        }
-        return {responseHeaders: headers};
-    },
-    {
-        urls: [ '*://*/*' ], // Pattern to match all http(s) pages
-        types: [ 'sub_frame' ]
-    },
-    ['blocking', 'responseHeaders']
-);
-
-console.log(encodeURIComponent(window.location.href));
-
-document.getElementById("forum_embed").src =
-  "https://groups.google.com/forum/embed/?place=forum/test-feed" +
-  "&parenturl=" +
-  encodeURIComponent(window.location.href); 
-
+  var pre = document.getElementById('upcoming-events');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
 }
 
-return{
+return {
 	onload: function() {
 		getUserInfo(false);
 		showTime();
@@ -258,9 +252,9 @@ return{
     gapi.client.load('calendar', 'v3', getCalendarSession);
 		//gapi.client.setApiKey('AIzaSyA8HYbU7zeqt58whlZiHpgI37b14pdFb9o');
 		$("#submit").on("click", sendEmail);
+    $("#submit-shoutout").on("click", sendShoutout);
 	//	{callback: gapi.client.load('gmail', 'v1') };
-		tryPrivate();
-	}
+}
 };
 })();
 
