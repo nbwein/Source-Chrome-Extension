257f7b583e564cec2f2c064cadc2847d7052040a
Maddie Cripps <madeline.cripps@stellaservice.com>
2015-07-06 14:19:36 -0400

    date and time

--- scripts/message-board.js
+++ scripts/message-board.js
@@ -4,7 +4,6 @@
 /* Load the RSS feed from Google Groups, display in the message board */
 function loadFeed(){
 	var rand = Math.trunc(Math.random() * 10000);
-	console.log(rand);
 	$.ajax({
 		method: "GET",
 		url: "https://groups.google.com/forum/feed/test-feed/msgs/rss_v2_0",
@@ -14,10 +13,49 @@ function loadFeed(){
 			var items = data.split("<item>");
 			delete items[0];
 			var container = document.getElementById("message-board");
+			var lastDay = "today";
+			var todayPrinted = false;
 			for (var i = 1; i < items.length; i++){
 				var elements = items[i].split("</title>");
 				var subject = elements[0].replace("<title>", "");
 				if (subject != "Lunch"){
+					var pubdate = elements[1].split("<pubDate>")[1].split("</pubDate>")[0];
+					var ampm = "am";
+					var datetime= pubdate.split(/\d{4}/);
+					var time = datetime[1];
+					time = time.split(":");
+					time[0] = time[0] - 4;
+					if (time[0] > 12){
+						time[0] = time[0] - 12;
+						ampm = "pm";
+					}
+					time = time[0] + ":" + time[1];
+					var today = new Date();
+					today.setHours(0,0,0,0);
+					today = today.toString();
+					date = Date.parse(pubdate);
+					date = new Date(date);
+					date = new Date(date.setHours(0,0,0,0));
+					if (today == date){
+						var day = "today";
+						if (!todayPrinted){
+							var div = document.createElement("div");
+                                                	div.appendChild(document.createTextNode(day));
+                                                	container.appendChild(div);
+							div.className = "date-bar";
+							todayPrinted = true;
+						}
+					}
+					else {
+						var day = datetime[0];
+					}
+					if (day != lastDay){
+						var div = document.createElement("div");
+						div.appendChild(document.createTextNode(day));
+						container.appendChild(div);
+						div.className = "date-bar"
+						lastDay = day;
+					}				
 					var description = elements[1].split("<description>")[1].split("</description>")[0];
 					var idparse = description.split("user_id: ");
 					var id = idparse[1];
@@ -26,7 +64,6 @@ function loadFeed(){
 					author = author.split("@")[0];
 					author_split = author.split(".");
 
-					console.log(author_split);
 
 					if (typeof author_split[1] != 'undefined'){
 						author_split[0] = author_split[0][0].toUpperCase() + author_split[0].slice(1); 
@@ -45,9 +82,13 @@ function loadFeed(){
 					auth.setAttribute("id", "message-author");
 					auth.innerHTML = author;
 					div.appendChild(auth);
-
 					var br = document.createElement("br");
 					div.appendChild(br);
+					t = document.createElement("span");
+					t.className = "message-time";
+					t.setAttribute("style", "float:right;");
+					t.innerHTML = time + " " + ampm;
+					div.appendChild(t);
 					// div.appendChild(document.createTextNode(entry)); 
 					var message = document.createElement("span");
 					message.setAttribute("style", "position:relative; padding-top: 10px");
@@ -72,7 +113,6 @@ function loadFeed(){
 							}
 						})
 						.done(function(data) {
-							console.log(entry);
 							pic.setAttribute("src", data.image.url);
 							pic.setAttribute("style", "position:relative;display:block;float:left;border-radius:50%;left:10px;bottom:5px");
 							div.appendChild(pic);
@@ -112,10 +152,8 @@ function getProfilePicture(id){
                 },
 		success: function(resp){
 			 url = resp.image.url;
-			 console.log(url);
 		},
 		error: function(resp){
-			console.log(resp);
 			return '';
 		}
 	});
